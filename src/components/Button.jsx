1addbb9c5e5012a216b7b35657283d009f9c06ad
Darun Karas <darunkaras@Daruns-MacBook-Air.local>
2025-02-15 11:03:11 +0600

    fixing the bg color of Button components

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,6 +1,7 @@
 export default function Button({ text, icon, color, background }) {
     return (
-        <button className={`btn flex justify-center items-center gap-1 hover:bg-[#ff7134] border-none bg-[${background}] text-${color}`}>
+        <button className={`btn flex justify-center items-center gap-1 hover:bg-[#ff7134] border-none text-${color}`}
+            style={{ backgroundColor: background }}>
             {text} {icon}
         </button>
     )
